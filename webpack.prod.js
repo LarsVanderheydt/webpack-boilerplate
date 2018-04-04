4c190c16b4f42b9986678a28ea2545315e256462
LarsVanderheydt <lars06@hotmail.be>
2018-04-04 14:17:21 +0200

    zeit command for deployment on ZEIT.co

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   devtool: 'source-map',
@@ -12,5 +13,11 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
+    new CopyWebpackPlugin([
+      {
+        from: `./src/now.json`,
+        to: `./`,
+      },
+    ]),
   ],
 });
